233e6eabf6ca81f7069ba40450e9a28053f07420
Miloš Slađan Davitković <davitkooo@gmail.com>
2016-03-02 11:23:30 +0100

    First commit

---
+++ script/measurements.js
@@ -0,0 +1,17 @@
+$(init);
+
+function init() {
+
+
+    $(".table-display-frame").css({
+        "display": "none"
+    });
+
+    $(".content").css({
+        "display": "block"
+    });
+
+    $(".toolbar-configuration").css({
+        "border": "2px solid red"
+    })
+}
