96f9a3a77da4609119036382765b993f67281f3f
Miloš Slađan Davitković <davitkooo@gmail.com>
2016-03-03 15:24:46 +0100

    Toolbar-ver 4

--- script/measurements.js
+++ script/measurements.js
@@ -15,3 +15,19 @@ function init() {
         "border": "2px solid red"
     })
 }
+
+
+//function addClassByClick(button) {
+//    $(button).addClass("active")
+//}
+//
+//$(function () { //run when the DOM is ready
+//    $(".toolbar-item").click(function () { //use a class, since your ID gets mangled
+//        $(this).addClass("active"); //add the class to the clicked element
+//    });
+//});
+
+$('.toolbar-item').click(function () {
+    //    $(this).addClass("active-toolbar-item");
+    $(this).toggleClass('active-toolbar-item');
+});
